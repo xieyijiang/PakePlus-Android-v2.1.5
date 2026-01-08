fa46f35f10a052df11c4442cbd427aa0c7ced6fe
Crabby <390094305@qq.com>
2026-01-08 13:47:27 +0800

    update src file from pakeplus

---
+++ scripts/www/assets/reload-CRn6K1K7.js
@@ -0,0 +1,8 @@
+
+/**
+ * 由 Fantastic-mobile 提供技术支持
+ * Powered by Fantastic-mobile
+ * https://fantastic-mobile.hurui.me/
+ */
+    
+import{d as t,M as n,O as a,_,s as c,o as p}from"./index-oMllnZjX.js";const u=t({__name:"reload",setup(s,{expose:o}){o();const e=n();a(()=>{e.go(-1)});const r={router:e};return Object.defineProperty(r,"__isScriptSetup",{enumerable:!1,value:!0}),r}});function d(s,o,e,r,i,l){return p(),c("div")}const m=_(u,[["render",d],["__file","/Users/crab7/develop/basic/src/views/reload.vue"]]);export{m as default};
